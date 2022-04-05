87490d8397c2ea9cad37df21099fbfb5b63e8e82
a_szczepanik <a.szczepanik@adform.com>
2022-04-05 11:34:17 +0200

    Spark up and running

--- src/main/scala/SparkDemo.scala
+++ src/main/scala/SparkDemo.scala
@@ -1,3 +1,12 @@
 object SparkDemo extends App {
-  println("Hello")
+
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder()
+    .appName("Spark SQL basic example")
+    //    .config("spark.some.config.option", "some-value")
+    .master("local[*]")
+    .getOrCreate()
+
 }
